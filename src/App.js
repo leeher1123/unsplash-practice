88672809a72848d41cc64776c9766858c503d092
leeher1123 <leeher1123@naver.com>
2021-08-08 17:17:37 +0900

    리셋 스타일 추가

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {Route, Switch} from 'react-router-dom';
 
 import Main from "./views/pages/Main";
 import Header from "./views/components/common/Header";
+import {GlobalStyle} from "./style/GlobalStyle";
 
 const App = () => {
   return (
     <Container>
+      <GlobalStyle/>
       <Header/>
       <Switch>
         <Route exact path={'/'} component={Main}></Route>
