873da582ddedb50307ccc1075c82b1904069e02b
leeher1123 <leeher1123@naver.com>
2021-08-08 14:23:28 +0900

    헤더 구성 컴포넌트 생성

---
+++ src/views/components/common/SearchBox/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchBox = () => {
+    return (
+        <Container>
+            SearchBox
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default SearchBox;
