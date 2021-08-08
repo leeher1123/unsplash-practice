476dd11d216ec30ee5ec2517e0c7639297a79b82
leeher1123 <leeher1123@naver.com>
2021-08-08 21:59:04 +0900

    헤더 메뉴 컴포넌트 구현

--- src/views/components/common/Header/Menu.js
+++ src/views/components/common/Header/Menu.js
@@ -1,16 +1,63 @@
 import React from "react";
 import styled from "styled-components";
+import {IconSpot} from "../../../../icons";
+import {DefaultButton} from "../Button/Button.Styled";
 
 const Menu = () => {
-    return (
-        <Container>
-            Menu
-        </Container>
-    )
+  return (
+    <Container>
+      <Text>
+        <a href="#">
+          <div>Brand</div>
+        </a>
+        <a href="#">
+          <div>Explore</div>
+        </a>
+        <a href="#">
+          <div>Blog</div>
+        </a>
+        <Button>
+          <IconSpot/>
+        </Button>
+      </Text>
+    </Container>
+  )
 }
 
 const Container = styled.div`
+  height: 62px;
+`;
 
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 36px;
+  a {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 20px 12px;
+    font-size: 14px;
+    font-weight: 500;
+    transition: .2s;
+    &:hover {
+      color: #000;
+    }
+  }
+`;
+
+const Button = styled(DefaultButton)`
+  padding: 0 12px;
+  svg {
+    color: #767676;
+    fill: currentColor;
+    transition: .2s;
+    &:hover {
+      cursor: pointer;
+      color: #000;
+    }
+  }
 `;
 
 export default Menu;
