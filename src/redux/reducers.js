6334bc9a1e7f85525bda677dfb1ef81ffb0a3c12
leeher1123 <leeher1123@naver.com>
2021-08-10 10:58:42 +0900

    photos, search의 redux, saga 세팅 및 로직 추가

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,12 @@
 import {combineReducers} from "redux";
+import appReducer from './app/slice';
+import photosReducer from './photos/slice';
+import searchReducer from './search/slice';
 
 const reducers = combineReducers({
-
+  app: appReducer,
+  photos: photosReducer,
+  search: searchReducer
 });
 
 export default reducers;
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,5 +1,14 @@
-function* sagas() {
+import {fork, all} from 'redux-saga/effects';
+import appSaga from './app/saga';
+import photosSaga from './photos/saga';
+import searchSaga from './search/saga';
 
+function* sagas() {
+  yield all([
+    fork(appSaga),
+    fork(photosSaga),
+    fork(searchSaga)
+  ])
 }
 
 export default sagas;
