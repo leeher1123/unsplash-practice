716124fdf0df9600a823363ebc8f7585acea0264
leeher1123 <leeher1123@naver.com>
2021-08-13 15:59:40 +0900

    Search Photos API 적용

--- src/redux/search/saga.js
+++ src/redux/search/saga.js
@@ -1,5 +1,14 @@
-function* saga() {
+import {call, put, takeLatest} from 'redux-saga/effects';
+import {Action} from "./slice";
+import API from "../../api";
 
+const searchPhotos = function* ({payload}) {
+  const result = yield call(API.searchPhotos, payload)
+  yield put(Action.Creators.setSearchResult(result.data))
+}
+
+function* saga() {
+  yield takeLatest(Action.Types.SEARCH_PHOTOS, searchPhotos)
 }
 
 export default saga;
--- src/views/containers/SearchContainer.js
+++ src/views/containers/SearchContainer.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
 import PhotoList from "../components/common/List/PhotoList";
+import SearchLnb from "../components/common/Lnb/SearchLnb";
+import {useDispatch, useSelector} from "react-redux";
+import {Action} from "../../redux/search/slice";
+import {Route, Switch, useParams} from "react-router-dom";
+import GridList from "../components/common/List/GridList";
+import CollectionItem from "../components/common/item/CollectionItem";
+import UserItem from "../components/common/item/UserItem";
 
 const SearchContainer = () => {
-  
-    return (
-        <Container>
-            <PhotoList/>
-        </Container>
-    )
+
+  const dispatch = useDispatch();
+  const {query} = useParams();
+  const {photos, collections, users, related_searches} = useSelector(state => state.search);
+
+  useEffect(() => {
+    searchPhotos();
+  }, [])
+
+  const searchPhotos = () => {
+    dispatch(Action.Creators.searchPhotos({
+      query,
+      per_page: 30
+    }))
+  }
+
+  const renderCollectionItem = (item, index) => <CollectionItem item={item} index={index} />
+  const renderUserItem = (item, index) => <UserItem item={item} index={index} />
+
+  return (
+    <Container>
+      <SearchLnb/>
+      <Switch>
+        <Route path={`/search/photos/${query}`}>
+          <PhotoList data={photos.results}/>
+        </Route>
+        <Route path={`/search/collections/${query}`}>
+          <GridList data={collections.results}
+                    renderItem={renderCollectionItem}
+          />
+        </Route>
+        <Route path={`/search/users/${query}`}>
+          <GridList data={users.results}
+                    renderItem={renderUserItem}
+          />
+        </Route>
+      </Switch>
+    </Container>
+  )
 }
 
 const Container = styled.div`
