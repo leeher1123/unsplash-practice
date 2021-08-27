36e3ff7a80fa0a24a4672f5120076b8b9d1e3084
leeher1123 <leeher1123@naver.com>
2021-08-27 15:26:05 +0900

    무한스크롤 구현

--- src/redux/photos/saga.js
+++ src/redux/photos/saga.js
@@ -1,13 +1,21 @@
 import {
-  takeLatest, all, call, put,
+  takeLatest, all, call, put, select,
 } from 'redux-saga/effects';
+import _ from 'lodash';
 
 import { Action } from './slice';
 import API from '../../api';
 
 const getPhotos = function* ({ payload }) {
   const result = yield call(API.getPhotos, payload);
-  yield put(Action.Creators.setPhotos(result.data));
+  if (_.isEmpty(result)) return;
+
+  const { photos } = yield select();
+  const nextPhotos = [
+    ...photos.list,
+    ...result.data,
+  ];
+  yield put(Action.Creators.setPhotos(nextPhotos));
 };
 
 function* saga() {
--- src/views/containers/MainPhotosContainer.js
+++ src/views/containers/MainPhotosContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,26 +6,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import PhotoList from '../components/common/List/PhotoList';
 import { Action } from '../../redux/photos/slice';
 import { ContentContainer } from '../components/common/Layout/Layout.Styled';
+import InfiniteScroll from '../components/common/InfiniteScroll';
 
 const MainPhotosContainer = () => {
+  const [page, setPage] = useState(1);
   const list = useSelector((state) => state.photos.list);
 
   const dispatch = useDispatch();
 
   const getPhotos = () => {
     dispatch(Action.Creators.getPhotos({
-      per_page: 30,
+      page,
+      per_page: 10,
     }));
+  };
+
+  const next = () => {
+    setPage((p) => p + 1);
   };
 
   useEffect(() => {
     getPhotos();
-  }, []);
+  }, [page]);
 
   return (
     <Container>
       <ContentContainer>
-        <PhotoList data={list} />
+        <InfiniteScroll
+          length={list.length}
+          next={next}
+        >
+          <PhotoList data={list} />
+        </InfiniteScroll>
       </ContentContainer>
     </Container>
   );
