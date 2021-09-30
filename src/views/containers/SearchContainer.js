e98b62a296b8aa4d34416067b3faf4f3c70bfe02
leeher1123 <leeher1123@naver.com>
2021-09-30 21:23:53 +0900

    filter 메뉴 api 연동

--- src/views/containers/SearchContainer.js
+++ src/views/containers/SearchContainer.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import qs from 'qs';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Route, Switch, useParams } from 'react-router-dom';
+import {
+  Route, Switch, useParams, useLocation,
+} from 'react-router-dom';
 
 import PhotoList from '../components/common/List/PhotoList';
 import SearchLnb from '../components/common/Lnb/SearchLnb';
@@ -18,20 +21,27 @@ import RelatedSearchesMenu from '../components/search/RelatedSearchesMenu';
 const SearchContainer = () => {
   const dispatch = useDispatch();
   const { query } = useParams();
+  const location = useLocation();
+
   const {
     photos, collections, users, related_searches,
   } = useSelector((state) => state.search);
+
+  const qsParams = qs.parse(location.search, { ignoreQueryPrefix: true });
 
   const searchPhotos = () => {
     dispatch(Action.Creators.searchPhotos({
       query,
       per_page: 30,
+      orientation: qsParams.orientation,
+      color: qsParams.color,
+      order_by: qsParams.order_by,
     }));
   };
 
   useEffect(() => {
     searchPhotos();
-  }, [query]);
+  }, [query, qsParams]);
 
   const renderCollectionItem = (item, index) => <CollectionItem item={item} index={index} />;
   const renderUserItem = (item, index) => <UserItem user={item} index={index} />;
