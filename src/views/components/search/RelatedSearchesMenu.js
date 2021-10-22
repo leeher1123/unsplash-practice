f25d237d742dcbc68dc0adafdd454037cc840d46
leeher1123 <leeher1123@naver.com>
2021-10-22 22:42:51 +0900

    RelatedSearchsMenu overflow 수정

--- src/views/components/search/RelatedSearchesMenu.js
+++ src/views/components/search/RelatedSearchesMenu.js
@@ -13,7 +13,13 @@ const RelatedSearchesMenu = ({ menu, initializeView }) => {
       <h1>{query}</h1>
       <ScrollMenu menu={menu}>
         {
-          (item) => <MenuItem to={`/search/photos/${item.title}`} onClick={initializeView}>{item.title}</MenuItem>
+          (item) => (
+            <MenuItem to={`/search/photos/${item.title}`} onClick={initializeView}>
+              <Item>
+                {item.title}
+              </Item>
+            </MenuItem>
+          )
         }
       </ScrollMenu>
     </Container>
@@ -34,10 +40,7 @@ const Container = styled.div`
 `;
 
 const MenuItem = styled(Link)`
-  display: flex;
   flex-shrink: 0;
-  align-items: center;
-  justify-content: center;
   border-radius: 4px;
   width: 143px;
   height: 40px;
@@ -45,18 +48,24 @@ const MenuItem = styled(Link)`
   font-weight: 500;
   text-transform: capitalize;
   color: #767676;
-  padding: 0 8px;
   border: 1px solid #d1d1d1;
   transition: .3s;
   cursor: pointer;
   & + & {
     margin-left: 8px;
   }
-  
   &:hover {
     color: #111;
     border-color: #111;
   }
+`;
+
+const Item = styled.div`
+  text-align: center;
+  padding: 13px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export default RelatedSearchesMenu;
